1f7efef0f633a1da9e330c78160423e4b6e18df6
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-06-22 21:54:58 +0200

    Updated plugins

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -6,8 +6,8 @@ vim.cmd [[:tnoremap <Esc> <C-\><C-n>]]
 
 -- disable numbers in terminal
 vim.api.nvim_create_autocmd('TermOpen', {
-  pattern = '',
-  command = "setlocal nonumber norelativenumber"
+    pattern = '',
+    command = "setlocal nonumber norelativenumber"
 })
 
 
@@ -23,4 +23,32 @@ return {
             vim.g.startuptime_tries = 10
         end,
     },
+    {
+        dir = 'C:\\Programming\\Lua\\Hypersonic.nvim',
+        --config = true,
+        config = function()
+            require('hypersonic').setup({
+                add_padding = false,
+                hl_group = 'boolean',
+                wrapping = ''
+            })
+
+            vim.keymap.set('v', '<leader>r', ":Hypersonic <CR>", { silent = true })
+        end
+    },
+--     {
+--         'nyngwang/NeoWell.lua',
+--         config = function()
+--             require('neo-well').setup {
+--                 height = 10
+--             }
+--
+--             vim.keymap.set('n', '<leader>b', ':NeoWellToggle <CR>', { silent = true })
+--             vim.keymap.set('n', '<leader>a', ':NeoWellAppend <CR>', { silent = true })
+--             vim.keymap.set('n', '<Tab>', function ()
+--   -- vim.cmd('NeoZoomToggle') -- remove this if you don't know what it is
+--   vim.cmd('NeoWellJump')
+-- end, { noremap = true, silent = true, nowait = true })
+--         end
+--     }
 }
