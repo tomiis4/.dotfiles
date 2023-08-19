c203800df94358dee5ee897b85066e9ea2942e0d
tomiis4 <tomas.kudynek@gmail.com>
2023-08-19 23:04:03 +0200

    update(nvim): statusline

--- nvim/lua/core/statusline.lua
+++ nvim/lua/core/statusline.lua
@@ -1,35 +1,8 @@
-local icons = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+local fileName = "  %{fnamemodify(expand('%'), ':~:.')}"
+local fileType = " %r%m%y"
+local separator = "%="
+local curLine = "[%l"
+local maxLine = "/%L]"
+local column = " %4v  "
 
-local function diagnostic_LSP()
-    local s = ''
-
-    local error = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
-    local warn = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-    local info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-    local hint = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
-
-    for k, v in pairs({ Error = error, Warn = warn, Info = info, Hint = hint }) do
-        if v > 0 then
-            local data = string.format('%s%s', icons[k], v)
-
-            s = string.format('%s%s ', s, data)
-        end
-    end
-
-    return s
-end
-
-local filename = "  %{expand('%:p:~:.:h')}/%{expand('%:t')}"
-local isSaved = ' %m'
-local split = '%='
-local lsp = ''
-local lines = '[%l/%L]  '
-
-vim.api.nvim_create_autocmd('DiagnosticChanged', {
-    callback = function()
-        lsp = diagnostic_LSP()
-        vim.o.statusline = string.format('%s%s%s%s%s%s', filename, isSaved, split, lsp, split, lines)
-    end
-})
-
-vim.o.statusline = string.format('%s%s%s%s%s%s', filename, isSaved, split, lsp, split, lines)
+vim.o.statusline = fileName .. fileType .. separator .. curLine .. maxLine .. column
