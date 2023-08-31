af51a0afa1d391733ade80eeb46d93bc59b79bf9
tomiis4 <tomas.kudynek@gmail.com>
2023-08-31 20:04:17 +0200

    add(nvim): hop.nvim keymap

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -8,12 +8,18 @@ local function hint_char()
     hop.hint_char1()
 end
 
+local function hint_patterns()
+    local hop = require('hop')
+    hop.hint_patterns()
+end
+
 return {
     'phaazon/hop.nvim',
     branch = 'v2',
     keys = {
         { 'f', hint_words },
         { 'F', hint_char },
+        { '<Tab>f', hint_patterns },
     },
     config = function()
         require('hop').setup {
