83572397a52d0fd5a7ee0be3cddbdd33394a83fc
tomiis4 <tomas.kudynek@gmail.com>
2023-09-05 20:54:41 +0200

    add(nvim-core): input window

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -5,16 +5,5 @@ require('core.transparent')
 require('core.completion')
 
 -- working on
-local x = require('core.utils.input')
-x.input({
-    title = 'x',
-    position = 'center',
-    border = 'rounded',
-    size = {
-        height = 0.5,
-        width = 0.5,
-    },
-    content = {},
-    }, function (option, _)
-        vim.print(option)
-    end)
+-- local x = require('core.utils.input')
+-- local y = require('core.utils.select')
